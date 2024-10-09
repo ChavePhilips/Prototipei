5535ad97ace8cafde353a407645609ed4714583d
ChavePhilips <felipe.sanchez.inamine@gmail.com>
2024-10-09 20:24:19 -0300

    atualização Rotas

--- src/js/rotas.js
+++ src/js/rotas.js
@@ -1,20 +1,2 @@
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
-var trak = document.getElementById('progress');
-var step = document.getElementById('step');
 
-next.addEventListener('click', function(){
-	var cls = trak.className.split('-').pop();
-	cls > 6 ? cls = 0 : cls++;
-  
-  step.innerHTML = cls;
-	trak.className = 'progress-' + cls;
-});
 
-prev.addEventListener('click', function(){
-	var cls = trak.className.split('-').pop();
-	cls < 1 ? cls = 7 : cls--;
-  
-  step.innerHTML = cls;
-	trak.className = 'progress-' + cls;
-});
