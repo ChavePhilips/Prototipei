ebdd4aab0466edb9476cd7530232c687befa5ad8
Felipe Inamine <felipe.sanchez.inamine@gmail.com>
2024-11-22 10:37:14 -0300

    rework carrinho

--- src/js/javascriptPadrao.js
+++ src/js/javascriptPadrao.js
@@ -22,28 +22,66 @@ for (i = 0; i < acc.length; i++) {
 const qt_e = parseInt(localStorage.getItem("qt_e")); // Quantidade da estufa
 const qt_p = parseInt(localStorage.getItem("qt_p")); // Quantidade da porta
 
-// BOTÃO +/- 
-document.getElementById('increase').addEventListener('click', function() {
-    let quantityInput = document.getElementById('quantity');
-    let currentValue = parseInt(quantityInput.value);
-
-    // Verifica se a quantidade atual + 1 é menor ou igual ao estoque disponível
+// Botão +/- rework
+function mudarQuantidade(button, input) {
+  let quantity = document.getElementById(input);
+  let currentValue = parseInt(quantity.value);
+  if (button.dataset.maisMenos == 1) {
     if (currentValue < qt_e) {
-        quantityInput.value = currentValue + 1;
+      quantity.value = currentValue + 1;
     } else {
-        // Bloqueia o incremento se não houver itens suficientes no estoque
-        alert("Não há itens suficientes no estoque para adicionar mais!");
-    }
-});
-
-document.getElementById('decrease').addEventListener('click', function() {
-    let quantityInput = document.getElementById('quantity');
-    let currentValue = parseInt(quantityInput.value);
-
+      // Bloqueia o incremento se não houver itens suficientes no estoque
+      alert("Não há itens suficientes no estoque para adicionar mais!");
+  }
+  } else {
     if (currentValue > 1) {
-        quantityInput.value = currentValue - 1;
+      quantity.value = currentValue - 1;
     }
-});
+  }
+}
+
+
+
+function todosOsItens() {
+  //CHECHBOX TODOS OS ITENS
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  var idTodosOsItens = document.getElementById('todosOsItens');
+
+  if (idTodosOsItens.checked) {
+      checkboxes.forEach(function(checkbox) {
+          checkbox.checked = true;
+      });
+  } else {
+      checkboxes.forEach(function(checkbox) {
+          checkbox.checked = false;
+      });
+  }
+}
+
+
+
+// BOTÃO +/- 
+// document.getElementById('increase').addEventListener('click', function() {
+//     let quantityInput = document.getElementById('quantity');
+//     let currentValue = parseInt(quantityInput.value);
+
+//     // Verifica se a quantidade atual + 1 é menor ou igual ao estoque disponível
+//     if (currentValue < qt_e) {
+//         quantityInput.value = currentValue + 1;
+//     } else {
+//         // Bloqueia o incremento se não houver itens suficientes no estoque
+//         alert("Não há itens suficientes no estoque para adicionar mais!");
+//     }
+// });
+
+// document.getElementById('decrease').addEventListener('click', function() {
+//     let quantityInput = document.getElementById('quantity');
+//     let currentValue = parseInt(quantityInput.value);
+
+//     if (currentValue > 1) {
+//         quantityInput.value = currentValue - 1;
+//     }
+// });
 
 // Função para selecionar o pagamento
 function selecionarPagamento(id) {
